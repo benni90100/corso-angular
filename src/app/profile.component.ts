e6cc66b55eb512620cedb4ff5d4cc783fe82a8ee
unknown <micelibenedetto800@gmail.com>
2025-01-29 19:11:17 +0100

    fist_commit

---
+++ src/app/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    standalone: true,
+    selector: 'app-profile',
+    template:'<h1>ciao sono Bennni</h1>',
+    styles: ['h1 { font-weight: normal; background-color: #f4f4f4; }']
+})
+export class ProfileComponent{}
+
